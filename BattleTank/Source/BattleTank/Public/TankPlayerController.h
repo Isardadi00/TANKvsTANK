7565dbae30fa56cfcfbbccd63c1bfcb674ee946d
Isardadi00 <isardadi0@gmail.com>
2018-11-06 17:48:54 +0000

    Successfully created an AI controller and a player controller so that when I create a firing and aiming mechanism for both tanks, the AI will succesfully attempt to hunt the player down

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -1,20 +1,22 @@
+#pragma once
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Tank.h"
 #include "GameFramework/PlayerController.h"
 #include "TankPlayerController.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-	
+public:
+	ATank * GetControlledTank() const;
+
+	virtual void BeginPlay() override;
 	
 	
 };
