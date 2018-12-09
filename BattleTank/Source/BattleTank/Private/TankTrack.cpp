864786fdc3f1211599f0e6bafdbed4d5524126ab
Isardadi00 <isardadi0@gmail.com>
2018-12-09 20:52:09 +0000

    Tank now moves more like a tank and doesn´t slide to the side

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -10,8 +10,14 @@ UTankTrack::UTankTrack()
 }
 void UTankTrack::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction * ThisTickFunction)
 {
-	Super::TickComponent;
-	UEÖLO
+	//Calculate the slippage speed
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+	// work-out the required acceleration this frame to correct
+	auto CorrectingAcceleration = (-SlippageSpeed / DeltaTime) * GetRightVector();
+	// calculate and apply sideways force ( F = m*a)
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CorrectionForce = (TankRoot->GetMass() * CorrectingAcceleration) / 2;
+	TankRoot->AddForce(CorrectionForce);
 }
 void UTankTrack::SetThrottle(float Throttle)
 {
